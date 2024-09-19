d4483c147e340fee662b3c04e419ce5731ad94b4
Sumit Mishra <sm0141136@gmail.com>
2024-09-19 20:36:00 +0530

    Increment and Decrement project

--- src/App.js
+++ src/App.js
@@ -1,43 +1,35 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-import Item from './Components/Item';
-import ItemDate from './Components/ItemDate';
-
-
 function App() {
-  const response=[
-    {
-      itemName:"Nirma",
-      itemDate:"20",
-      itemMonth:"June",
-      itemyear:"1998"
-
-    },
-    {
-      itemName:"Nirma2",
-      itemDate:"20",
-      itemMonth:"June2",
-      itemyear:"1998"
-    },
-    {
-      itemName:"Nirma3",
-      itemDate:"203",
-      itemMonth:"June3",
-      itemyear:"19983"
-    },
-  ]
+  const[count, setCount]=useState(0);
+  function decreaseHandler(){
+    setCount(count - 1); 
+  }
+  function increaseHandler(){
+    setCount(count+1);
+  }
+  function resetHandler(){
+    setCount(0);
+  }
   return (
-    <div>
-      <Item name={response[0].itemName}></Item>
-      <ItemDate  day={response[0].itemDate} month={response[0].itemMonth} year={response[0].itemyear}></ItemDate>
-
-      <Item name={response[1].itemName}></Item>
-      <ItemDate  day={response[1].itemDate} month={response[1].itemMonth} year={response[1].itemyear}></ItemDate>
-
-
-      <Item name={response[2].itemName}></Item>
-      <ItemDate  day={response[2].itemDate} month={response[2].itemMonth} year={response[2].itemyear}></ItemDate>
-      <div className="App">Hello jii</div>
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10">
+      <div className="text-[#0398d4] font-medium text-2xl">Increment && Decrement</div>
+      <div className="bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151]">
+        <button onClick={decreaseHandler} className="border-r-2 text-center w-20 border-[#bfbfbf] text-5xl">
+          -
+        </button>
+        <div className="font-bold gap-12 text-5xl">
+          {count}
+        </div> 
+        <button onClick={increaseHandler} className="border-r-2 text-center w-20 border-[#bfbfbf] text-5xl">
+          +
+          </button>
+      </div>
+      <div>
+        <button onClick={resetHandler} className="bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg">
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
