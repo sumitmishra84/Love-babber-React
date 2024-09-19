c5a2624ba5ef279b89155c4da368c25cb1d460a2
Sumit Mishra <sm0141136@gmail.com>
2024-09-19 09:46:30 +0530

    React Basic=01

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import Item from './Components/Item';
+import ItemDate from './Components/ItemDate';
+
 
 function App() {
+  const response=[
+    {
+      itemName:"Nirma",
+      itemDate:"20",
+      itemMonth:"June",
+      itemyear:"1998"
+
+    },
+    {
+      itemName:"Nirma2",
+      itemDate:"20",
+      itemMonth:"June2",
+      itemyear:"1998"
+    },
+    {
+      itemName:"Nirma3",
+      itemDate:"203",
+      itemMonth:"June3",
+      itemyear:"19983"
+    },
+  ]
   return (
-    <div className='"Appp'>
-      Hello ji
+    <div>
+      <Item name={response[0].itemName}></Item>
+      <ItemDate  day={response[0].itemDate} month={response[0].itemMonth} year={response[0].itemyear}></ItemDate>
+
+      <Item name={response[1].itemName}></Item>
+      <ItemDate  day={response[1].itemDate} month={response[1].itemMonth} year={response[1].itemyear}></ItemDate>
+
+
+      <Item name={response[2].itemName}></Item>
+      <ItemDate  day={response[2].itemDate} month={response[2].itemMonth} year={response[2].itemyear}></ItemDate>
+      <div className="App">Hello jii</div>
     </div>
   );
 }
-
 export default App;
---
+++ src/Components/Item.js
@@ -0,0 +1,7 @@
+import './Item.css';
+
+function Item(props){
+     const itemName=props.name;
+     return(<p className="nirma">{itemName}</p>);
+}
+export default Item;
